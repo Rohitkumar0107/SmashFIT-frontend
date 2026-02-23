606608359503daad77ccc868c5aa6ca898b810c8
Rohit <rohitkumar964930@gmail.com>
2026-02-23 20:39:26 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/SmashFIT-frontend",
   server: {
     port: 5173,
     headers: {
